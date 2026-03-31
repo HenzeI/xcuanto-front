9107bbef130015539898a94a5e9aa9223a4c033a
hancel <hancel.abrines@geoaitech.com>
2026-03-31 02:57:11 +0200

    feat: cambios para desplegar prototipo en github pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,4 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  base: '/xcuanto-front/',   // ← cambia esto por el nombre exacto de tu repo en GitHub
 })
